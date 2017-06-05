aef2617b2604427d18746b3d03d58e48fc433060
Yanci Martinez <yanci@localhost.localdomain>
2017-06-04 18:13:55 -0600

    parcial_3

--- parcial_3/build/cmake_install.cmake
+++ parcial_3/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/yanci/Documentos/ALGORITMOS GRAFICOS/TAREA_COMPUTO2/parcial3
+# Install script for directory: /home/yanci/Documentos/ALGORITMOS GRAFICOS/proyectos/parcial_3
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -38,7 +38,7 @@ else()
   set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
 endif()
 
-file(WRITE "/home/yanci/Documentos/ALGORITMOS GRAFICOS/TAREA_COMPUTO2/parcial3/build/${CMAKE_INSTALL_MANIFEST}" "")
+file(WRITE "/home/yanci/Documentos/ALGORITMOS GRAFICOS/proyectos/parcial_3/build/${CMAKE_INSTALL_MANIFEST}" "")
 foreach(file ${CMAKE_INSTALL_MANIFEST_FILES})
-  file(APPEND "/home/yanci/Documentos/ALGORITMOS GRAFICOS/TAREA_COMPUTO2/parcial3/build/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
+  file(APPEND "/home/yanci/Documentos/ALGORITMOS GRAFICOS/proyectos/parcial_3/build/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
 endforeach()
